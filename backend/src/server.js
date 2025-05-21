9c6fb4086ef35f5f3f2c8a636e8340a589498f66
imrosun <imrosun@gmail.com>
2025-05-21 19:35:09 +0530

    update port for dynamic

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ const { PORT, MONGO_URI } = require('./config');
 const app = require('./app.js');
 const startEmailScheduler = require('./jobs/emailScheduler.js');
 
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
 mongoose.connect(MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
